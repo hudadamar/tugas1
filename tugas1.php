41b1a67ff26e2350fb4b5f6802d096cf8fa10424
hudadamar <48892003+hudadamar@users.noreply.github.com>
2019-04-01 14:18:59 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,20 @@
+<?php
+    echo"<h1>Hello World</h1> <br/>";
+    
+    $text = "<table>
+    <tr>
+        <td>NIM</td>
+        <td> : </td>
+        <td>16.01.53.0124/td>
+    </tr>
+    <tr>
+        <td>Nama</td>
+        <td> : </td>
+        <td>Huda Damar Sangkara</td>
+    </tr>
+</table> <br>";
+
+    echo $text;
+?>
+
+    <a href="/akademik">Go To Akademik</a>
